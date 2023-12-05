b69cfa0ff202c51012d2a1ccec39be2661d796a1
nhattranb <s3926629@rmit.edu.vn>
2023-12-05 15:29:44 +0700

    add data fetching functions

---
+++ db/fetch.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const Product = require('../models/product');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://TungTT:ySAn8E1flmR7oOzY@cluster0.rtiqjut.mongodb.net/CommercialWebsite', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.render('/public/views/pages/product.ejs', {products: products});
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('500 Server Error');
+    }
+});
+
+module.exports = router;
